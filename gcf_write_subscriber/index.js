2835b6ad0adb28dc317cdfe4b20f65d652fd77da
abmnq <fireballthered@gmail.com>
2024-03-25 11:54:13 +0000

    Initial project commit

---
+++ gcf_write_subscriber/index.js
@@ -0,0 +1,22 @@
+const {Firestore} = require('@google-cloud/firestore');
+
+exports.SubscriberFB = async (message, context) =>{
+    const firestore = new Firestore({
+        projectId: 'ahmoha-traveldeals'
+    });
+    try {
+    const incoming = Buffer.from(message.data,'base64').toString('utf-8');
+    const parsedMsg = JSON.parse(incoming);
+    const subscriberData = {
+        email_address: parsedMsg.email_address,
+        watch_regions: parsedMsg.watch_region
+    };
+    let collectionRef = firestore.collection('subscribers');
+    const docRef = await collectionRef.add(subscriberData);
+    console.log(`New subscriber document added with ID: ${docRef.id}`);
+} catch (error) {
+    console.error(`Error adding document to Firestore: ${error}`);
+}
+};
+
+
--- web/app.js
+++ web/app.js
@@ -2,11 +2,38 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 8080;
+const bodyParser = require('body-parser');
+const { PubSub } = require('@google-cloud/pubsub')
 
+
+//middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use (bodyParser.json());
+
+//variable
+const pubsub_topic = "travel_deals_signup";
 // ROUTES
 app.get('/', (req, res) => {
   //res.status(200).send('Hello, world!');
   res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.post('/subscribe', async (req, res) => {
+  const email = req.body.email_address;
+  const regions = req.body.watch_region;
+
+const pubSubClient = new PubSub();
+
+const message_data = JSON.stringify({
+  email_address: email,
+  watch_region: regions
+});
+
+const dataBuffer = Buffer.from(message_data);
+//publish message
+const messageID = await pubSubClient.topic(pubsub_topic).publishMessage({data: dataBuffer});
+console.log(`Message ID: ${messageID}`);
+res.status(200).send(`Thanks for signing up for TraveDeals. <br/>Message ID: ${messageID}`);
 });
 
 app.listen(port, () => {
